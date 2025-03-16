d699696995124333f6cd925bba26b407cf8da888
sahilcodes <sahilcancode@gmail.com>
2025-03-16 17:58:18 +0530

    Initial push

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "SahilCodes",
+      status: "active",
+      tasks: ["Task One", "Task Two", "Task Three"]
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+</template>
