f11efa914119718ca91d6438c6af96002ebf4c6a
sahilcodes <sahilcancode@gmail.com>
2025-03-17 23:10:02 +0530

    Added placeholder TODO example with Compostion API for learning purpose as of now.

--- src/App.vue
+++ src/App.vue
@@ -30,11 +30,12 @@ export default {
 </script> -->
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 const name = ref("SahilCodes");
 const status = ref("active");
 const tasks = ref(["Task One", "Task Two", "Task Three"]);
+const newTask = ref("");
 
 const toggleStatus = () => {
   if (status.value === "active") {
@@ -45,6 +46,29 @@ const toggleStatus = () => {
     status.value = "active";
   }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map((task) => {
+      return task.title;
+    });
+  } catch (error) {
+    console.log("Error", error);
+  }
+});
 </script>
 
 <template>
@@ -53,13 +77,23 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">X</button>
+    </li>
   </ul>
 
   <!-- <a v-bind:href="link">Click here</a> -->
   <a :href="link">Click for GitHub Profile</a>
   <br />
-  <button @:click="toggleStatus">Change Status</button>
+  <button @click="toggleStatus">Change Status</button>
 </template>
